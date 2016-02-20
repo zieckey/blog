e7d848449ddc3f1838ea3ca80eb2345d7322a8b3
weizili.laptop <zieckey@gmail.com>
2016-02-20 22:39:50 +0800

    rebuilding site Sat Feb 20 22:39:50     2016

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,31 @@
-#
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-hugo --theme=hugo-rapid-theme
-sleep 1
-mv public/ public.new
-git co master
-cp public.new/* . -rf
-rm public.new/ -rf
-git st
-git add *
-git st
-git ci -a -m'new release'
-git push
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+# Push Hugo content
+git add -A
+git commit -m "$msg"
+git push origin hugo
+
+
+# Build the project.
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
